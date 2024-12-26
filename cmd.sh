a6383b5784fb0a00c578b38c4d2e07a99073b3c1
techcto <root@Dark2>
2024-12-26 16:56:26 -0500

    ..

--- cmd.sh
+++ cmd.sh
@@ -7,19 +7,24 @@ tag(){
     git push --tags
 }
 
+wsl(){
+    source vllm/bin/activate
+}
+
 wsl2(){
     python3 -m venv vllm
     source vllm/bin/activate
     python3 -m pip install --upgrade pip setuptools wheel
     python3 -m ensurepip --upgrade
-    sudo apt-key del 7fa2af80
+
+    apt-key del 7fa2af80
     wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
-    sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
-    wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-wsl-ubuntu-11-8-local_11.8.0-1_amd64.deb
-    sudo dpkg -i cuda-repo-wsl-ubuntu-11-8-local_11.8.0-1_amd64.deb
-    sudo cp /var/cuda-repo-wsl-ubuntu-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
-    sudo apt-get update
-    sudo apt-get -y install cuda
+    mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/3bf863cc.pub
+    add-apt-repository 'deb https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/ /'
+    apt-get update
+    apt-get -y install cuda
+
     nvcc --version
     python3 -m pip install vllm
 }
